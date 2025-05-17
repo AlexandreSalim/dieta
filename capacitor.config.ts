0fa1ea2a0133dded0a9cda83061f84925ccc0dab
MENEZES Matheus S <matheus.menezes@hexagon.com>
2025-05-17 17:07:46 -0300

    app name alter

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.caloriadebolso',
-  appName: 'Caloria de bolso',
+  appName: 'Calorias de bolso',
   webDir: 'www',
   plugins: {
     PushNotifications: {
