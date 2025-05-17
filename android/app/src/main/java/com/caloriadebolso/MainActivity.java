26a41c4ebafd5cf065dbb3f262b00219aef5cd20
MENEZES Matheus S <matheus.menezes@hexagon.com>
2025-05-17 14:55:08 -0300

    config android

---
+++ android/app/src/main/java/com/caloriadebolso/MainActivity.java
@@ -0,0 +1,5 @@
+package com.caloriadebolso;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.caloriadebolso',
   appName: 'teste_com_payment',
   webDir: 'www',
   plugins: {
